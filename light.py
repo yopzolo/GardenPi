99b960fe478602d55155357eeca811fc4a6ff0c3
yopZolo <paul.blin@gmail.com>
2013-04-23 03:14:07 +0200

    IHM de config dynamique

--- light.py
+++ light.py
@@ -1,9 +1,12 @@
 # Imports
 import webiopi
 import time
+import json
 
 webiopi.setDebug()
 GPIO = webiopi.GPIO
+
+duration = 1;
 
 LIGHT   = 23
 
@@ -13,7 +16,24 @@ def setup():
 def loop():
     value = not GPIO.digitalRead(LIGHT)
     GPIO.digitalWrite(LIGHT, value)
-    webiopi.sleep(5)        
+    webiopi.sleep(duration)        
 
 def destroy():
     webiopi.debug("Script with macros - Destroy")
+
+
+
+@webiopi.macro
+def setConfig(inDuration):
+    global duration;
+    duration = int(inDuration);
+
+@webiopi.macro
+def getConfig():
+
+	configLigth = {'ligth_start': 10, 'ligth_duration': 8};
+	configPump = {'pump_cycle_day' : 30, 'pump_duration_day' : 30, 'pump_cycle_nigth' : 60, 'pump_duration_nigth' : 60}
+	
+	config = [configLigth, configPump];
+		
+	return json.dumps(config);
