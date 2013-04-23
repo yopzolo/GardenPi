719c56c5f38d9eb3405479d9d7ab16bc2a4fce2c
paul blin <paulblin@iMac.local>
2013-04-23 10:34:15 +0200

    setConfig ok

--- light.py
+++ light.py
@@ -24,16 +24,16 @@ def destroy():
 
 
 @webiopi.macro
-def setConfig(inDuration):
+def setConfig(ligth_start, ligth_duration, pump_cycle_day, pump_duration_day, pump_cycle_nigth, pump_duration_nigth, fan_trigger_temp_day, fan_trigger_temp_night):
     global duration;
-    duration = int(inDuration);
+    duration = int(ligth_duration);
 
 @webiopi.macro
 def getConfig():
-
 	configLigth = {'ligth_start': 10, 'ligth_duration': 8};
 	configPump = {'pump_cycle_day' : 30, 'pump_duration_day' : 30, 'pump_cycle_nigth' : 60, 'pump_duration_nigth' : 60}
-	
-	config = [configLigth, configPump];
-		
+	configFan = {'fan_trigger_temp_day' : 27 , 'fan_trigger_temp_night': 21};
+
+	config = {'ligth' : configLigth, 'pump' : configPump, 'fan' : configFan};
+
 	return json.dumps(config);
