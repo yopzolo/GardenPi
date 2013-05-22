7c2429236411bb81e275e1204bd97d22c2abf09c
yopzolo <paul.blin@gmail.com>
2013-05-22 03:08:03 +0200

    Ajout config et runner pour animer la lampe et la pompe

---
+++ config.py
@@ -0,0 +1,67 @@
+from datetime import datetime, time, timedelta
+
+#
+# data updater
+#
+class ConfigRunner(object):
+
+    def __init__(self):
+        self.runner = SetRunner()
+
+    def update(self, config, state):
+        self.updateAtTime(config, state, datetime.now())
+
+    def updateAtTime(self, config, state, time):
+        if time.time() > config.configSets[0].startTime or time.time() < config.configSets[1].startTime:
+            currentConfig = config.configSets[0]
+            state.day = True
+        else:
+            currentConfig = config.configSets[1]
+            state.day = False;
+
+        self.runner.updateAtTime(currentConfig, state, time)
+
+class SetRunner(object):
+    def __init__(self):
+        self.pumpRunner = PeriodicRunner()
+
+    def updateAtTime(self, config, state, time):
+        state.pump = self.pumpRunner.valueAtTime(config.pumpPeriod, time)
+
+class PeriodicRunner(object):
+    def __init__(self):
+        self.lastOn = datetime(1970,1,1,0,0,0)
+
+    def valueAtTime(self, periodicConfig, time):
+        if time >= self.lastOn + timedelta(minutes=periodicConfig.period):
+            self.lastOn = time
+            return True
+        if time >= self.lastOn + timedelta(minutes=periodicConfig.duration):
+            return False
+            
+        return True;
+        
+#
+# state storage
+#
+class RegisterState(object):
+    def __init__(self):
+        self.day = False
+        self.pump = False
+
+#
+# data storage
+#
+class ConfigRoot(object):
+    def __init__(self):
+        self.configSets = [ConfigSet(), ConfigSet()]
+
+class ConfigSet(object):
+    def __init__(self):
+        self.startTime = time.min
+        self.pumpPeriod = PeriodicConfig()
+
+class PeriodicConfig(object):
+    def __init__(self):
+        self.period = 0
+        self.duration = 0
---
+++ configTest.py
@@ -0,0 +1,76 @@
+import unittest
+import json
+
+from datetime import datetime, time
+
+from config import ConfigRoot, ConfigRunner, RegisterState
+from config import ConfigSet, SetRunner
+from config import PeriodicConfig, PeriodicRunner
+
+class SetRunnerMock(SetRunner):
+    def __init__(self):
+        self.config = 0
+
+    def updateAtTime(self, config, state, time):
+        self.config = config
+
+class ConfigTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def assertConfig(self, runner, date, state, expectedDay, config, expectedConfig):
+        runner.updateAtTime(config, state, date)
+        self.assertEqual(runner.runner.config, expectedConfig)
+        self.assertEqual(state.day, expectedDay)        
+ 
+    def test_configRoot(self):
+        config = ConfigRoot()
+
+        dayConfig = config.configSets[0]
+        dayConfig.startTime = time(17,0,0)
+        
+        nightConfig = config.configSets[1]
+        nightConfig.startTime = time(5,0,0)
+
+        runner = ConfigRunner()
+        runner.runner = SetRunnerMock()
+
+        current = RegisterState()
+        
+        self.assertConfig(runner, datetime(1981,11,2,13,0,0), current, False, config, nightConfig)
+        self.assertConfig(runner, datetime(1981,11,2,16,0,0), current, False, config, nightConfig)
+        self.assertConfig(runner, datetime(1981,11,2,17,1,0), current, True, config, dayConfig)
+        self.assertConfig(runner, datetime(1981,11,2,19,0,0), current, True, config, dayConfig)
+        self.assertConfig(runner, datetime(1981,11,3,1,0,0), current, True, config, dayConfig)
+        self.assertConfig(runner, datetime(1981,11,3,6,0,0), current, False, config, nightConfig)
+        self.assertConfig(runner, datetime(1981,11,3,15,0,0), current, False, config, nightConfig)
+        self.assertConfig(runner, datetime(1981,11,3,18,0,0), current, True, config, dayConfig)
+        
+    def test_configSet(self):
+        pass
+
+    def test_period(self):
+        config = PeriodicConfig()
+
+        config.period = 3
+        config.duration = 1
+
+        runner = PeriodicRunner()
+        for minNb in range(0, 59):
+            self.assertEqual(runner.valueAtTime(config, datetime(1981,11,2,17,minNb,0)), minNb%3==0)
+
+    def test_period_skipUpdates(self):
+        config = PeriodicConfig()
+        config.period = 5
+        config.duration = 2
+
+        expected = [True, True, False, False, False, True, True, False, False, False, True, True, False, False, False, True, True]
+
+        runner = PeriodicRunner()
+        self.assertEqual(runner.valueAtTime(config, datetime(1981,11,2,17,0,0)), expected[0])
+        self.assertEqual(runner.valueAtTime(config, datetime(1981,11,2,17,0,0)), expected[0])
+        self.assertEqual(runner.valueAtTime(config, datetime(1981,11,2,17,11,0)), expected[11])
+
+if __name__ == '__main__':
+    unittest.main()
