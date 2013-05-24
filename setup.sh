85a0e6365fe6bb76db2cc85427c56d869375b980
yopZolo <paul.blin@gmail.com>
2013-05-24 11:51:36 +0200

    config de prod correct

--- setup.sh
+++ setup.sh
@@ -6,19 +6,19 @@ HOME=/usr/share/webiopi/htdocs/
 SENSOR_HOME=WebIOPi-0.6.0/python/webiopi/devices/sensor
 SENSOR_INIT=$SENSOR_HOME/__init__.py
 
-# echo "downloading webiopi"
-# wget http://webiopi.googlecode.com/files/WebIOPi-0.6.0.tar.gz
-# tar xvzf WebIOPi-0.6.0.tar.gz
+echo "downloading webiopi"
+wget http://webiopi.googlecode.com/files/WebIOPi-0.6.0.tar.gz
+tar xvzf WebIOPi-0.6.0.tar.gz
 
-# echo "adding DHT to webiopi module"
-# cp DHT_X/Adafruit_DHT /usr/bin/
-# cp DHT_X/dht11.py $SENSOR_HOME
-# cp DHT_X/sensor_init.py $SENSOR_INIT
+echo "adding DHT to webiopi module"
+cp DHT_X/Adafruit_DHT /usr/bin/
+cp DHT_X/dht11.py $SENSOR_HOME
+cp DHT_X/sensor_init.py $SENSOR_INIT
 
-# echo "installing webiopi"
-# cd WebIOPi-0.6.0
-# ./setup.sh
-# cd ..
+echo "installing webiopi"
+cd WebIOPi-0.6.0
+./setup.sh
+cd ..
 
 echo "copying g4rdenP1 configuration"
 cp config $CONFIG_FILE
@@ -30,4 +30,4 @@ cp garden.py $HOME
 cp config.py $HOME
 
 /./etc/init.d/webiopi restart
-#sudo webiopi -c /etc/webiopi/config
+# webiopi -c /etc/webiopi/config
