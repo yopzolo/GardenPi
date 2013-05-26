665b2ef928a0f526253dea937db1b7d46293bbd9
yopZolo <paul.blin@gmail.com>
2013-05-26 16:25:51 +0200

    Nettoyage config lors de reinstallation

--- setup.sh
+++ setup.sh
@@ -6,21 +6,24 @@ HOME=/usr/share/webiopi/htdocs/
 SENSOR_HOME=WebIOPi-0.6.0/python/webiopi/devices/sensor
 SENSOR_INIT=$SENSOR_HOME/__init__.py
 
-echo "downloading webiopi"
+echo "Downloading webiopi"
 wget http://webiopi.googlecode.com/files/WebIOPi-0.6.0.tar.gz
 tar xvzf WebIOPi-0.6.0.tar.gz
 
-echo "adding DHT to webiopi module"
+echo "Adding DHT to webiopi module"
 cp DHT_X/Adafruit_DHT /usr/bin/
 cp DHT_X/dht11.py $SENSOR_HOME
 cp DHT_X/sensor_init.py $SENSOR_INIT
 
-echo "installing webiopi"
+echo "Removing old g4rdenP1 configuration"
+rm $HOME/GardenPyConfig.pyc
+
+echo "Installing webiopi"
 cd WebIOPi-0.6.0
 ./setup.sh
 cd ..
 
-echo "copying g4rdenP1 configuration"
+echo "Copying g4rdenP1 configuration"
 cp config $CONFIG_FILE
 
 cp DHT_X/dht11.js $HOME
